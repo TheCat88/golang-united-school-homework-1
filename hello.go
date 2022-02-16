fb4adf2b46c0ba16b01267c3ef7419d1f34d031c
EP <panarin.evg@gmail.com>
2022-02-16 20:31:07 +0300

    hello6

--- hello.go
+++ hello.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/kyokomi/emoji"
 )
+
+func main() {
+	var a = emoji.Sprintf(":world_map:!")
+	fmt.Printf(emoji.Sprintf("Hello " + strings.ReplaceAll(a, " ", "")))
+}
 
 func GetMessage() string {
 	var a = emoji.Sprintf(":world_map:!")
